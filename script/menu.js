971580b946770a72f764e347ddd2ac50ab634f82
Raikkonen Lee <raikkonenlee@RaikkonentekiMacBook-Pro.local>
2017-03-09 01:12:23 +0800

    調整首頁

--- script/menu.js
+++ script/menu.js
@@ -16,11 +16,18 @@
         var subMenuNumber = target.getAttribute("data-menu");
         var subMenuObject = document.getElementsByClassName("submenu-" + subMenuNumber)[0];
         //
-        if (subMenuObject.classList.contains("hidden")) {
-            subMenuObject.classList.remove("hidden");
-            RemoveHidden(parseInt(subMenuNumber));
-        } else {
-            subMenuObject.classList.add("hidden");
+        if (subMenuObject !== undefined) {
+            ChangeActiveState();
+            if (subMenuObject.classList.contains("hidden")) {
+                //指定點選的做動態效果
+                target.classList.add("active");
+                target.children[0].classList.remove("triangle-down");
+                target.children[0].classList.add("triangle-up");
+                subMenuObject.classList.remove("hidden");
+                RemoveHidden(parseInt(subMenuNumber));
+            } else {
+                subMenuObject.classList.add("hidden");
+            }
         }
     }
 
@@ -32,6 +39,31 @@
             if (keepMenuNumber !== (i0 + 1)) {
                 if (!element.classList.contains("hidden")) {
                     element.classList.add("hidden");
+                }
+            }
+        }
+    }
+
+    function ChangeActiveState() {
+        var menu = [];
+        var menu1 = document.getElementsByClassName("menu1");
+        var menu2 = document.getElementsByClassName("menu2");
+        menu = menu.concat(menu1);
+        menu = menu.concat(menu2);
+        //清除所有的動態效果
+        for (var i0 = 0; i0 < menu.length; i0++) {
+            var ulTag = menu[i0][0].children[0].children;
+            for (var q0 = 0; q0 < ulTag.length; q0++) {
+                var aTag = ulTag[q0].children[0];
+                var iTag = aTag.children[0];
+                if (aTag.classList.contains("active")) {
+                    aTag.classList.remove("active");
+                }
+                if (iTag !== undefined) {
+                    if (iTag.classList.contains("triangle-up")) {
+                        iTag.classList.remove("triangle-up");
+                        iTag.classList.add("triangle-down");
+                    }
                 }
             }
         }
