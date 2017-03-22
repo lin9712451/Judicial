b4706ef8096c4b0edf8235ed7fac6f1202c299b6
Raikkonen Lee <raikkonenlee@RaikkonentekiMacBook-Pro.local>
2017-03-22 11:46:37 +0800

    調整menu

--- script/menu.js
+++ script/menu.js
@@ -1,36 +1,44 @@
 
 (function menu (){
-    var menu1, menu2;
+    var menu1, menu2, menuBar, menuClose;
     var content_menu, content_title_container;
     menu1 = document.getElementsByClassName("menu1")[0];
     menu2 = document.getElementsByClassName("menu2")[0];
-    // content_menu = document.getElementsByClassName("content-menu");
-    // content_title_container = 
-    //     document.getElementsByClassName("content-title-container");
+    menuBar = document.getElementsByClassName("menu-bar")[0];
+    menuClose = document.getElementsByClassName("menu-close")[0];
     //
     if (document.addEventListener){
         menu1.addEventListener("click", MenuToggle, false);
         menu2.addEventListener("click", MenuToggle, false);
-        // if (content_menu !== undefined) {
-        //     window.addEventListener("scroll", StickSubMenuAndSubTitle, false);
-        // }
+        menuBar.addEventListener("click", OpenAndCloseMenu, false);
+        menuClose.addEventListener("click", function(){
+            OpenAndCloseMenu("close");
+        } , false);
     } else if (document.attachEvent) {
         menu1.attachEvent("onclick", MenuToggle);
-        // window.attachEvent("onscroll", StickSubMenuAndSubTitle);
+        menu2.attachEvent("click", MenuToggle);
+        menuBar.attachEvent("click", OpenAndCloseMenu);
+        menuClose.addEventListener("click", function(){
+            OpenAndCloseMenu("close");
+        });
     }
     //
     function MenuToggle(event) {
         var target = event.target || event.srcElement;
-        var subMenuNumber = target.getAttribute("data-menu");
-        var subMenuObject = document.getElementsByClassName("submenu-" + subMenuNumber)[0];
+        var subMenuNumber, subMenuObject;
         //
+        if (target.tagName !== "A") {
+            target = target.parentNode;
+        }
+        subMenuNumber = target.getAttribute("data-menu");
+        subMenuObject = document.getElementsByClassName("submenu-" + subMenuNumber)[0];
         if (subMenuObject !== undefined) {
             ChangeActiveState();
             if (subMenuObject.classList.contains("hidden")) {
                 //指定點選的做動態效果
                 target.classList.add("active");
-                target.children[0].classList.remove("triangle-down");
-                target.children[0].classList.add("triangle-up");
+                target.children[0].classList.remove("triangle-close");
+                target.children[0].classList.add("triangle-open");
                 subMenuObject.classList.remove("hidden");
                 RemoveHidden(parseInt(subMenuNumber));
             } else {
@@ -68,17 +76,31 @@
                     aTag.classList.remove("active");
                 }
                 if (iTag !== undefined) {
-                    if (iTag.classList.contains("triangle-up")) {
-                        iTag.classList.remove("triangle-up");
-                        iTag.classList.add("triangle-down");
+                    if (iTag.classList.contains("triangle-open")) {
+                        iTag.classList.remove("triangle-open");
+                        iTag.classList.add("triangle-close");
                     }
                 }
             }
         }
     }
 
-    // function StickSubMenuAndSubTitle() {
-
-    // }
+    function OpenAndCloseMenu(action) {
+        var titlebarLeft = document.getElementsByClassName("titlebar-left")[0];
+        var menuCollection = document.getElementsByClassName("menu-collection")[0];
+        //
+        if (action === "close") {
+            titlebarLeft.style.display = "none";
+            menuCollection.style.display = "none";
+        } else {
+            if (window.getComputedStyle(titlebarLeft).display === "none") {
+                titlebarLeft.style.display = "block";
+                menuCollection.style.display = "block";
+            } else {
+                titlebarLeft.style.display = "none";
+                menuCollection.style.display = "none";
+            }
+        }
+    }
 })();
 
