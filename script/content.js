0ccb07629335b80c26ddc594c2433dea41647d17
RaikkonenLee <raikkonenlee@RaikkonenMac.local>
2017-04-10 16:34:33 +0800

    修正css反應的問題

--- script/content.js
+++ script/content.js
@@ -33,3 +33,30 @@
         }
     }
 })();
+
+(function ChangeContentParagraph() {
+    var contentParagraph = document.getElementsByClassName("content-paragraph")[0];
+    var insideUl = contentParagraph.children[0];
+    //
+    if (document.addEventListener) {
+        insideUl.addEventListener("click", ChangeActive, false);
+    } else {
+        insideUl.attachEvent("onclick", ChangeActive);
+    }
+    
+    function ChangeActive(event) {
+        var target = event.target || event.srcElement;
+        var tagName = target.tagName;
+        //
+        if (tagName === "A") {
+            for (var i0 = 0; i0 < target.parentNode.parentNode.children.length; i0++) {
+                var element = target.parentNode.parentNode.children[i0];
+                element.classList.remove("active");
+            }
+            target.parentNode.classList.add("active");
+        } else {
+            event.preventDefault();
+            return false;
+        }
+    }
+})();
